6a4a07f4a3957ffbb01b95c91e86e4fb968a28e2
Swapnil <swapnilw8451@gmail.com>
2024-04-28 21:03:46 +0530

    first

---
+++ src/main/java/in/ashokit/controller/MessageController.java
@@ -0,0 +1,26 @@
+package in.ashokit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+	@GetMapping("/welcome")
+	public String getWelcomeMsg(Model model) {
+
+		model.addAttribute("msg", "Welcome To Thymeleaf App");
+
+		return "index";
+	}
+
+	
+	// BUG - 123
+
+	@GetMapping("/greet")
+	public String getGreetMsg(Model model) {
+
+		// In progrees
+	}
+}
